f2faaebf6b9e36266cc1da2d5583031ec608e5ea
JoYehyun <jo990714@naver.com>
2023-11-04 21:22:12 +0900

    style: 코드 정리

--- src/test/kotlin/racingcar/InputValidatorTest.kt
+++ src/test/kotlin/racingcar/InputValidatorTest.kt
@@ -1,7 +1,6 @@
 package racingcar
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import utils.InputValidator.checkCarNames
